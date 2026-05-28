cf902fa1ec63a3c3af7437a3033180f41508f35a
melognf <melognf@yahoo.com.ar>
2026-05-28 14:06:51 -0300

    datos

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // ══════════════════════════════════════════
 
 // BUILD: 2026-05-28T20:00:00  ← este timestamp se reescribe en cada deploy
-const BUILD = '2026-05-28T13:48:13';
+const BUILD = '2026-05-28T14:06:50';
 const CACHE_NAME = `team-days-${BUILD}`;
 
 const ASSETS = [
